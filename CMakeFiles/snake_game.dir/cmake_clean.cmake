b25915e733fb6db58b667a0a922d4cc726cdd917
Nguyen Cong Sang <cs.ncsang@gmail.com>
2021-03-31 19:12:46 +0700

    first commit

---
+++ CMakeFiles/snake_game.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/snake_game.dir/src/snake_game.cpp.o"
+  "snake_game"
+  "snake_game.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/snake_game.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
